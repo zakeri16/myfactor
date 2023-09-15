1ce70c8c180f12bec0f0a0f0081d74d79169e9d0
zakeri16 <102593803+zakeri16@users.noreply.github.com>
2023-09-15 16:07:45 +0330

    file uploaded

---
+++ src/main.js
@@ -0,0 +1,45 @@
+const resultEle = document.getElementById("result-body");
+const submitBtn = document.getElementById("submit-btn");
+const sumEle = document.getElementById("sum");
+
+let sum = 0;
+
+submitBtn.onclick = function () {
+  const customerNameEle = document.getElementById("customerName");
+  const customerNumberEle = document.getElementById("customerNumber");
+  const productName = document.getElementById("productName");
+  const productPrice = document.getElementById("productPrice");
+  const productQTY = document.getElementById("productQTY");
+  /*const newTR = document.createElement("tr");
+        const tdProductName = document.createElement("td");
+        tdProductName.innerText = productName;
+        newTR.appendChild(tdProductName);
+        resultEle.appendChild(newTR);*/
+  let currentSum = productPrice.value * productQTY.value;
+  sum += currentSum;
+
+  let newRecord = "<tr>";
+  newRecord += "<td>" + productName.value + "</td>";
+  newRecord += "<td>" + productPrice.value + "</td>";
+  newRecord += "<td>" + productQTY.value + "</td>";
+  newRecord += "<td>" + currentSum + "</td>";
+  newRecord += "</tr>";
+  resultEle.innerHTML += newRecord;
+
+  customerNameEle.readonly = "readonly";
+  customerNameEle.disabled = "disable";
+
+  customerNumberEle.readonly = "readonly";
+  customerNumberEle.disabled = "disable";
+
+  productName.value = "";
+  productPrice.value = "";
+  productQTY.value = "";
+
+  sumEle.innerText = "مجموع قابل پرداخت :" + sum;
+};
+
+const printBtn = document.getElementById("print-btn");
+printBtn.onclick = function () {
+  window.print();
+};
